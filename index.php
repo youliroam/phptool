7ba4a7c43e7c1d1c224eb730ebeb11e5b6a90137
youli <youliroam@163.com>
2020-06-01 18:26:36 +0800

    第一次提交，一个初始化的框框

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use youliroam\phptool\Tools;
+Tools::index();
---
+++ src/Youliroam/Phptool/Tools.php
@@ -0,0 +1,15 @@
+<?php
+namespace youliroam\phptool;
+
+class Tools{
+	
+	public function __construct(){
+
+
+	}
+
+	public static function index(){
+
+		echo 'youliroam/phptool php tool class';
+	}
+}
